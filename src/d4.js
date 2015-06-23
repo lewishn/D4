909a869b2f270fbe03fa612a22c34cc08ca57db7
Lewis <lewis.haris@gmail.com>
2015-06-23 10:09:01 +0800

    Add pie chart

--- src/d4.js
+++ src/d4.js
@@ -21,6 +21,8 @@ angular.module('d4', []).directive('d4', function($window) {
 			
 			if (options.chart === "horizontalBar") {
 				drawHorizontalBarChart(svg, data, options, element);
+			} else if (options.chart === "pieChart") {
+				drawPieChart(svg, data, options, element);
 			}
 		}
 
@@ -74,5 +76,55 @@ angular.module('d4', []).directive('d4', function($window) {
 			.attr('y', function(d,i) { return fontSize + i * (fontSize + textPadding + barHeight + barPadding);})
 			.style("font-size", fontSize)
 			.text(function(d) { return d.label + " (" + d.value +" votes)"});
-	}  
+	}
+
+	function drawPieChart(svg, data, options, element) {
+		// if no data passed, exit
+		if (!data) return;
+		
+		// set up variables
+		var outerRadius = options.outerRadius / 100 || 0.9,
+			innerRadius = options.innerRadius / 100 || 0,
+			offsetX = options.offsetX || 0,
+			offsetY = options.offsetY || 0,
+			showLabels = options.showLabels || true,
+			strokeColor = options.strokeColor || 'white',
+			margin = options.margin || 0,
+			width = options.width || d3.select(element[0]).node().offsetWidth - margin,
+			height = options.height || 500,
+			min = Math.min(width, height),
+			color = d3.scale.category20();
+
+		svg.attr("width", width)
+			.attr("height", height);
+
+		var pie = d3.layout.pie()
+			.sort(null)
+			.value(function(d){ return d.value; });
+		
+		var arc = d3.svg.arc()
+		        .outerRadius(min / 2 * outerRadius)
+		        .innerRadius(min / 2 * innerRadius);
+
+		var g = svg.append('g')
+			.attr('transform', 'translate(' + (width / 2 + offsetX) + ',' + (height / 2 + offsetY) + ')');
+
+		// add the paths for each arc slice
+		var slice = g.selectAll('path')
+			.data(pie(data));
+
+		slice.enter()
+			.append('path')
+			.attr('d', arc)
+			.style('stroke', strokeColor)
+	    	.style('fill', function(d, i){ return color(i) });
+
+	    // pie labels
+	    slice.enter()
+	    	.append("text")
+			.attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+			.attr("dy", ".5em")
+			.style("text-anchor", "middle")
+			.text(function(d) { return d.data.label + "(" + d.data.value +")"});
+	} 
 });
