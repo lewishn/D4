a41855cf01f3edb471dd759efa35007c50676d8c
Lewis <lewis.haris@gmail.com>
2015-06-25 17:48:07 +0800

    Add option for secondary charts

--- src/d4.js
+++ src/d4.js
@@ -1,12 +1,12 @@
 'use strict';
-
-angular.module('d4', []).directive('d4', function($window) {
+app.directive("d4", function($window) {
 	return {
 		link: link,
 		restrict: "E",
 		scope: {
 			data: '=',
-			options: '='
+			options: '=',
+			images: '='
 		}
 	}
 
@@ -18,18 +18,26 @@ angular.module('d4', []).directive('d4', function($window) {
 		scope.render = function(data, options) {
 			// remove all previous items before render
 			svg.selectAll('*').remove();
+			var ops = setupOptions(options, element);
 			
-			if (options.chart === "horizontalBar") {
-				drawHorizontalBarChart(svg, data, options, element);
-			} else if (options.chart === "pie") {
-				drawPieChart(svg, data, options, element);
-			} else if (options.chart === "bubble") {
-				drawBubbleChart(svg, data, options, element);
+			//draw primary
+			drawHorizontalBarChart(svg, data, ops, false);
+
+			//draw secondary
+			if (ops.secondary === 'horizontalBar') {
+				drawHorizontalBarChart(svg, data, ops, true);
+			} else if (ops.secondary === 'pie') {
+				drawPieChart(svg, data, ops, true);
+			} else if (ops.secondary === 'bubble') {
+				drawBubbleChart(svg, data, ops, true);
 			}
 		}
 
         scope.$watch('data', function(newValues, oldValues) {
         	return scope.render(newValues, scope.options);
+		}, true);
+		scope.$watchCollection('options', function(newValues, oldValues) {
+        	return scope.render(scope.data, newValues);
 		}, true);
 
        // Browser resize event
@@ -45,77 +53,69 @@ angular.module('d4', []).directive('d4', function($window) {
 		});
 	}
 
-	function drawHorizontalBarChart(svg, data, options, element) {
+	function drawHorizontalBarChart(svg, data, options, isSecondary) {
 		// if no data passed, exit
 		if (!data) return;
-		
-		// set up variables
-		var barHeight = options.barHeight || 30,
-			barPadding = options.barPadding || 10,
-			margin = options.margin || 10,
-			fontSize = options.fontSize || 16,
-			textPadding = options.textPadding || 5,
-			width = options.width || d3.select(element[0]).node().offsetWidth - margin,
-			height = options.height || fontSize + data.length * (fontSize + textPadding + barHeight + barPadding),
-			color = d3.scale.category20(),
+
+		// set svg height
+		var height = options.fontSize + data.length * (options.fontSize + options.textPadding + options.barHeight + options.barPadding);
+		options.height = (options.height) ? Math.max(options.height, height) : height;
+		svg.attr('height', options.height);
+
+		var	color = d3.scale.category20(),
 			xScale = d3.scale.linear()
 				.domain([0, d3.max(data, function(d) { return d.value; })])
-				.range([0, width]);
+				.range([0, options.width - options.margin]);
 
-		svg.attr('height', height);
-
+		var g = svg.append("g");
 		// create the rectangles for the bar chart
-		var bar = svg.selectAll('rect').data(data);
+		var bar = g.selectAll('rect').data(data);
 		bar.enter().append('rect')
-			.attr('height', barHeight)
+			.attr('height', options.barHeight)
 			.attr('width', function(d) { return xScale(d.value); })
-			.attr('x', Math.round(margin/2))
-			.attr('y', function(d,i) { return textPadding + fontSize + i * (fontSize + textPadding + barHeight + barPadding);})
+			.attr('x', Math.round(options.margin/2))
+			.attr('y', function(d,i) { return options.textPadding + options.fontSize + i * (options.fontSize + options.textPadding + options.barHeight + options.barPadding);})
 			.attr('fill', function(d) { return color(d.label) });
 		
 		bar.enter().append("text")
-			.attr('x', Math.round(margin/2))
-			.attr('y', function(d,i) { return fontSize + i * (fontSize + textPadding + barHeight + barPadding);})
-			.style("font-size", fontSize)
+			.attr('x', Math.round(options.margin/2))
+			.attr('y', function(d,i) { return options.fontSize + i * (options.fontSize + options.textPadding + options.barHeight + options.barPadding);})
+			.style("font-size", options.fontSize)
 			.text(function(d) { return d.label + " (" + d.value +" votes)"});
-	}
 
-	function drawPieChart(svg, data, options, element) {
+		var translateX = isSecondary ? options.width : 0;
+		g.attr('transform', 'translate(' + translateX + ', 0)');
+	} 
+
+	function drawPieChart(svg, data, options, isSecondary) {
 		// if no data passed, exit
 		if (!data) return;
 		
 		// set up variables
-		var outerRadius = options.outerRadius / 100 || 0.7,
-			innerRadius = options.innerRadius / 100 || 0,
-			textRadius = options.textRadius || 1.1,
-			offsetX = options.offsetX || 0,
-			offsetY = options.offsetY || 0,
-			showLabels = options.showLabels || true,
-			strokeColor = options.strokeColor || 'white',
-			margin = options.margin || 0,
-			fontSize = options.fontSize || 16,
-			textFormat = options.textFormat || function(l, k) { return l + "(" + k + ")";},
-			width = options.width || d3.select(element[0]).node().offsetWidth - margin,
-			height = options.height || 500,
+		var textRadius = options.textRadius || 1.1,
+			width = options.width - options.margin,
+			height = Math.min(width, 400),
 			radius = Math.min(width, height) / 2,
 			color = d3.scale.category20();
 
-		svg.attr("width", width)
-			.attr("height", height);
+		if (options.height) {
+			svg.attr("height", Math.max(options.height, height));
+		}
 
 		var pie = d3.layout.pie()
 			.sort(null)
 			.value(function(d){ return d.value; });
 		
 		var arc = d3.svg.arc()
-		        .outerRadius(radius * outerRadius)
-		        .innerRadius(radius * innerRadius);
+		        .outerRadius(radius * options.outerRadius - options.margin)
+		        .innerRadius(radius * options.innerRadius - options.margin);
 		var textArc = d3.svg.arc()
-				.outerRadius(radius * outerRadius * 2 * textRadius)
-				.innerRadius(radius * innerRadius * 2 * textRadius);
+				.outerRadius(radius * options.outerRadius * 2 * textRadius - options.margin)
+				.innerRadius(0 - options.margin);
 
+		var translateX = (width / 2 + options.offsetX) + (isSecondary ? width : 0);
 		var g = svg.append('g')
-			.attr('transform', 'translate(' + (width / 2 + offsetX) + ',' + (height / 2 + offsetY) + ')');
+			.attr('transform', 'translate(' + translateX + ',' + (height / 2 + options.offsetY) + ')');
 
 		// add the paths for each arc slice
 		var slice = g.selectAll('path')
@@ -124,7 +124,7 @@ angular.module('d4', []).directive('d4', function($window) {
 		slice.enter()
 			.append('path')
 			.attr('d', arc)
-			.style('stroke', strokeColor)
+			.style('stroke', options.strokeColor)
 	    	.style('fill', function(d, i){ return color(i) });
 
 	    // pie labels
@@ -138,9 +138,9 @@ angular.module('d4', []).directive('d4', function($window) {
 	    labels.enter()
 	    	.append("text")
 			.attr("dy", "0.3em")
-			.style("font-size", fontSize)
+			.style("font-size", options.fontSize)
 			.text(function(d) { 
-				return textFormat(d.data.label, d.data.value);
+				return d.data.value > 0 ? d.data.label : "" ;
 			})
 			.attr("transform", function(d) {
 				var pos = textArc.centroid(d);
@@ -150,35 +150,27 @@ angular.module('d4', []).directive('d4', function($window) {
 			.style("text-anchor", function(d) {
 				return midAngle(d) < Math.PI ? "start" : "end";
 			});
-	} 
+	}  
 
-	function drawBubbleChart(svg, data, options, element) {
-		// set up variables
-		var padding = options.padding || 0,
-			fontSize = options.fontSize || 16,
-			fitHeight = options.fitHeight || false,
-			width = options.width || d3.select(element[0]).node().offsetWidth,
-			height = options.height || width,
-			showLabels = options.showLabels || true,
-			color = d3.scale.category20();
-
-		svg.attr("width", width)
-			.attr("height", height);
-
+	function drawBubbleChart(svg, data, options, isSecondary) {
+		var color = d3.scale.category20();
+		var height = Math.min(options.width, 400);
 		var pack = d3.layout.pack()
-			.size([width, height])
+			.size([options.width, height])
 			.padding(5);
 
 		var nodes = pack.nodes({children: angular.copy(data)});
 		// remove root
 		nodes.shift();
-		var top = fitHeight ? d3.min(nodes, function(d) { return d.y - d.r; }) : 0;
-		if (fitHeight) {
+		var top = options.fitHeight ? d3.min(nodes, function(d) { return d.y - d.r; }) : 0;
+		if (options.fitHeight) {
 			var bottom = d3.max(nodes, function(d) { return d.y + d.r; });
-			svg.attr("height", bottom - top);
+			var h = options.height || height;
+			svg.attr("height", Math.max(h, bottom - top));
 		}
 
-		var node = svg.selectAll(".node")
+		var g  = svg.append("g");
+		var node = g.selectAll(".node")
 			.data(nodes).enter()
 				.append("g")
 				.attr("class", "node")
@@ -188,55 +180,41 @@ angular.module('d4', []).directive('d4', function($window) {
 			.attr("fill", function(d) { return (d.children) ? "none" : color(d.label);})
 			.attr("r", function(d) { return d.r; });
 
-		if (showLabels) {
-			node.append("text")
-		      .attr("dy", ".3em")
-		      .style("font-size", fontSize)
-		      .style("text-anchor", "middle")
-		      .text(function(d) { return d.label.substring(0, 5) || d.label});
-	 	}
+		node.append("text")
+		    .attr("dy", ".3em")
+		    .style("font-size", options.fontSize)
+		    .style("text-anchor", "middle")
+		    .text(function(d) { return d.label.substring(0, 5) || d.label});
+
+		if (isSecondary) {
+			g.attr("transform", "translate(" + options.width + ",0 )");
+		}
 	}
 
-	function drawHeatTable(svg, data, options, element) {
-		var colorScale = d3.scale.linear()
-			.domain([d3.min(data, function (d) { return d.value; }), d3.max(data, function (d) { return d.value; })])
-			.range(["#1A237E", "#D50000"])
-			.interpolate(d3.interpolateHcl);
-
-		// transform data to table style
-		var tableData = [], rowData = [];
-		for (var i = 0; i < data.length; i++) {
-			if (i != 0 && i % col.length === 0) {
-				tableData.push(rowData);
-				rowData = [];
-			}
-			rowData.push(data[i]);
-		}
-
-		var table = d3.select(element[0]).append("table")
-			.attr("class", "table");
-		var thead = table.append("thead");
-		var tbody = table.append("tbody");
-
-		// append column headers
-		var headRow = thead.append("tr");
-		headRow.append("th");
-		headRow.selectAll("th.col").data(col)
-			.enter().append("th")
-				.text(function(d) { return d; });
-
-		// append row headers
-		var tr = tbody.selectAll("tr").data(tableData)
-			.enter().append("tr")
-				.append("th")
-				.text(function(d, i) {return row[i];});
-
-		tbody.selectAll("tr").each(function(data, i) {
-			d3.select(this).selectAll("td").data(data)
-				.enter().append("td")
-				.style("background-color", function(d) { return colorScale(d.value); })
-				.style("color", "#FFFFFF")
-				.text(function(d){ return d.value });
-		});
+	function setupOptions(options, element) {
+		var svgWidth = d3.select(element[0]).node().offsetWidth;
+		// Setup options
+		options.fontSize = options.fontSize || 16;
+		options.strokeColor = options.strokeColor || "white";
+		options.strokeWidth = options.strokeWidth || 2;
+		options.color = options.color || "steelblue";
+		// horizontal bar
+		options.barHeight = options.barHeight || 30;
+		options.barPadding = options.barPadding || 10;
+		options.textPadding = options.textPadding || 5;
+		options.margin = options.margin || 10;
+		// pie / donut
+		options.outerRadius = options.outerRadius || 0.9;
+		options.innerRadius = options.innerRadius || 0;
+		options.offsetX = options.offsetX || 0;
+		options.offsetY = options.offsetY || 0;
+		// bubble
+		options.padding = options.padding || 0;
+		options.fitHeight = options.fitHeight || true;
+		// heat table
+		console.log(options.secondary);
+		options.heatColor = options.heatColor || ["#1A237E" , "#DD2C00"];
+		options.width = (options.secondary) ? svgWidth / 2 : svgWidth;
+		return options;
 	}
 });
